c4b32d1ddc93280d475a15748d395eb9b1adb5a8
Zidan.Wang <zidan.wang@gmail.com>
2019-02-01 14:52:15 +0800

    add Settings project.

---
+++ src/Scorpio.Settings/Scorpio/Settings/ISettingEncryptionService.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scorpio.Settings
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISettingEncryptionService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="settingDefinition"></param>
+        /// <param name="plainValue"></param>
+        /// <returns></returns>
+        [CanBeNull]
+        string Encrypt([NotNull]SettingDefinition settingDefinition, [CanBeNull] string plainValue);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="settingDefinition"></param>
+        /// <param name="encryptedValue"></param>
+        /// <returns></returns>
+        [CanBeNull]
+        string Decrypt([NotNull]SettingDefinition settingDefinition, [CanBeNull] string encryptedValue);
+    }
+}
---
+++ src/Scorpio.Settings/Scorpio/Settings/ISettingManager.cs
@@ -0,0 +1,57 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scorpio.Settings
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISettingManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        Task<string> GetOrNullAsync([NotNull]string name);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="providerName"></param>
+        /// <param name="providerKey"></param>
+        /// <param name="fallback"></param>
+        /// <returns></returns>
+        Task<string> GetOrNullAsync([NotNull]string name, [NotNull] string providerName, [CanBeNull] string providerKey, bool fallback = true);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<List<SettingValue>> GetAllAsync();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="providerName"></param>
+        /// <param name="providerKey"></param>
+        /// <param name="fallback"></param>
+        /// <returns></returns>
+        Task<List<SettingValue>> GetAllAsync([NotNull] string providerName, [CanBeNull] string providerKey, bool fallback = true);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <param name="providerName"></param>
+        /// <param name="providerKey"></param>
+        /// <param name="forceToSet"></param>
+        /// <returns></returns>
+        Task SetAsync([NotNull] string name, [CanBeNull] string value, [NotNull] string providerName, [CanBeNull] string providerKey, bool forceToSet = false);
+    }
+}
